58ec4d9479c8bf4b0b250464e7019c7456732c4f
Nik <nikitama1hr@gmail.com>
2023-10-12 19:08:39 +0300

    draft letter collection

--- src/components/DetailsCategory.tsx
+++ src/components/DetailsCategory.tsx
@@ -1,6 +1,23 @@
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function DetailsCategory() {
+    const [collection, setCollection] = useState('')
+    const [letters, setLetters] = useState({
+        letters: [{}]
+    })
+    useEffect(() => {
+
+        async function reqCollectionLettersData() {
+            const collectionID = location.href.split("/")[5]
+            const fetchCollectionData = await fetch('http://localhost:3001/collection/' + collectionID)
+            const preparedJSON = await fetchCollectionData.json()
+            setLetters({letters: preparedJSON.message})
+        }
+
+        console.log(letters)
+        reqCollectionLettersData()
+    }, [])
     return (
         <div>
             <h2>COLLCTION NAME</h2>
@@ -12,24 +29,19 @@ export default function DetailsCategory() {
             </Link>
             <h4>Collection list</h4>
             <ul>
-                <li>
-                    <button>v</button>
-                    <button>✏</button>
-                    <button>x</button>
-                    <input type="text" value={'letter 3'} disabled={true}/>
-                </li>
-                <li>
-                    <button>v</button>
-                    <button>✏</button>
-                    <button>x</button>
-                    <input type="text" value={'letter 2'} disabled={true}/>
-                </li>
-                <li>
-                    <button>v</button>
-                    <button>✏</button>
-                    <button>x</button>
-                    <input type="text" value={'letter 1'} disabled={true}/>
-                </li>
+                {
+                    letters.letters.map((item) => {
+                        return (<li key={item.id}>
+                            <button>v</button>
+                            <button>✏</button>
+                            <button>x</button>
+                            <input type="text" value={item.term} disabled={true}/>
+                            <input type="text" value={item.tip} disabled={true}/>
+                            <input type="text" value={item.answer} disabled={true}/>
+                        </li>)
+                    })
+                }
+
             </ul>
         </div>
     )
